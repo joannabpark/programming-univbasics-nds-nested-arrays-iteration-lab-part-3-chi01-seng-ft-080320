ce10f58316042bf9c5c694f4bd39208674821db0
JoAnna Park <joannabpark1@gmail.com>
2020-07-17 23:41:18 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
     element_index = 0
     while element_index < src[row_index].length do
     if src[row_index][element_index].is_a? String
-     sentence << src[row_index][element_index]
+     sentence << src[row_index][element_index]('_')
   end
       element_index += 1
   end
